f3de35f13db13c39223ee91c7846907c3769ca12
Olawumi Olaibi <olatreasure90@gmail.com>
2023-04-27 16:20:58 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - tests function that print is integer is positive, negative, or zero
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * MAIN_H
+ * Description: Header file containing function prototypes
+ * Function Prototypes
+ * @i : integer to be tested
+ */
+
+void positive_or_negative(int i);
+int largest_number (int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int maonth, int day, int year);
+
+#endif
