1f48c967b366c2f8ede9aab8585dd8e28b3d3245
Olawumi Olaibi <olatreasure90@gmail.com>
2023-05-02 16:21:05 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Function to print every other character of a string
+ * @str: pointer to the string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[i++])
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
