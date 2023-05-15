1fb3e4576c6f6e696732dae1998f4865e2314768
Olawumi Olaibi <olatreasure90@gmail.com>
2023-05-15 14:38:31 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all the argument it receives
+ * @argc: Number or count of argument
+ * @argv: Array of pointer to the string
+ *
+ * Return: Alwayts 0
+ */
+
+int main(int argc, char **argv)
+{
+	int k;
+
+	for (k = 1; k < argc; k++)
+		printf("%s\n", argv[k]);
+
+	return (0);
+}
