92bf5d72aaab26f2b177412b02d3f2140228038e
Olawumi Olaibi <olatreasure90@gmail.com>
2023-05-22 14:28:38 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - Definining a new type of dog
+ * @name: First element
+ * @age: Second element
+ * @owner: Third element
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
